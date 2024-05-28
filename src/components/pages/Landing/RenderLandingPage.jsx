f38218870374c39a9e5e5f1b4dcf61560936e8c5
Michaiah Bos <michaiahbos@yahoo.com>
2024-05-28 15:50:06 -0500

    wip

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -68,6 +68,13 @@ function RenderLandingPage(props) {
         >
           View the Data
         </Button>
+    
+        <Button
+          type="default"
+          style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+        >
+          Download the Data
+        </Button>
       </div>
 
       <div className="middle-section">
